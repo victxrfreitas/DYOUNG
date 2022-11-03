81d453cbf347390758038f0c9a767369b07456a5
victxrfreitas <maciel.freitas@sptech.school>
2022-11-03 19:02:52 -0300

    Inclusão do Slack

---
+++ Java swing login/dyoung-project/src/main/java/com/mycompany/dyoung/project/Slack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dyoung.project;
+
+import com.azure.core.http.HttpClient;
+
+/**
+ *
+ * @author MU993HT
+ */
+public class Slack {
+    private static HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+}
