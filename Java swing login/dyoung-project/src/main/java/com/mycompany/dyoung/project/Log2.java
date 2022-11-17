281172532df3422be2979836d937aed9aa097e1e
MiguelMolina777 <miguel.molina@sptech.school>
2022-11-16 21:18:59 -0300

    helpdesk linkado

---
+++ Java swing login/dyoung-project/src/main/java/com/mycompany/dyoung/project/Log2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dyoung.project;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+/**
+ *
+ * @author XY379RX
+ */
+public class Log2 {
+//    DaoLogin login = new DaoLogin();
+// String user = login.usuario;
+// String data = LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE.ofLocalizedDate(FormatStyle.FULL));
+//
+//    // Método Salvar    
+//     public void Salvar (BeansUsuario beansus){
+//         conex.conectar();
+//    try {
+//        PreparedStatement pst = conex.con.prepareStatement
+//            ("insert into usuario(nome, usuario, senha)"
+//                    + " values(?,?,?)");
+//            pst.setString(1, beansus.getNome());
+//            pst.setString(2, beansus.getUsuario());
+//            pst.setString(3, beansus.getSenha());
+//            pst.execute();
+//            //
+//        PreparedStatement pst2 = conex.con.prepareStatement
+//            ("insert into atividades(usuario, acao, campo, data)"
+//                    + " values(?,?,?,?)"); 
+//            pst2.setString(1, user);
+//            pst2.setString(2, "Salvou o novo Usuario:");
+//            pst2.setString(3, beansus.getNome());
+//            pst2.setString(4, data);
+//            pst2.execute();
+//            JOptionPane.showMessageDialog(null, "Dados inseridos com Sucesso");
+//    } catch (SQLException ex) {
+//        JOptionPane.showMessageDialog(null, "Erro ao inserir Dados.\n Erro:"+ex);
+//    }
+//     conex.desconetar();
+// }
+}
